6a6589988d8ca90c473c2cacc9d9e24e844260cd
Lubana <lubanabu98@gmail.com>
2022-08-22 17:44:06 +0300

    preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 import Home from "@components/home";
 import Particle from "@components/ParticalBackground/Particl";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
   return (
     <>
       <Particle />
-      <Home />;
+      {!loading && <Home />}
     </>
   );
 }
